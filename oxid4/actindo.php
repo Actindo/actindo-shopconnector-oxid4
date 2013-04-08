99efa6c33cce18327968b7491c0387f27a829d1a
s-philipp <schmidt@actindo.de>
2013-04-08 11:38:18 +0200

    initial version-update

--- oxid4/actindo.php
+++ oxid4/actindo.php
@@ -8,11 +8,11 @@
  **
  * @package actindo
  * @author  Patrick Prasse <pprasse@actindo.de>
- * @version $Revision: 522 $
+ * @version $Revision: 523 $
  * @copyright Copyright (c) 2007, Patrick Prasse (Schneebeerenweg 26, D-85551 Kirchheim, GERMANY, pprasse@actindo.de)
  */
 
-define( 'ACTINDO_SHOPCONN_REVISION', '$Revision: 522 $' );
+define( 'ACTINDO_SHOPCONN_REVISION', '$Revision: 523 $' );
 define( 'ACTINDO_PROTOCOL_REVISION', '2.'.substr( ACTINDO_SHOPCONN_REVISION, 11, -2 ) );
 
 /* change dir into admin interface and include application_top.php */
@@ -666,7 +666,7 @@ function actindo_get_time( $params )
  */
 function shop_get_connector_version( &$response )
 {
-  $revision = '$Revision: 522 $';
+  $revision = '$Revision: 523 $';
   $response->set_revision( $revision );
   $response->set_protocol_version( '2.'.substr( $revision, 11, -2 ) );
   $response->set_shop_type( act_get_shop_type( ) );
